14115bfba39818f397dbc419e488b41bfcc6358a
BrainZipUser <62482338+BrainZipUser@users.noreply.github.com>
2022-10-26 23:36:16 +0300

    Movement
    Реалізована система переміщення

---
+++ Game/ShooterGame/Assets/Script/Character_Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    //створюємо ссилку на персонажа
+    [SerializeField] private FP_Character Character;
+    private void Start()
+    {
+        Character.LockCursor();
+    }
+    private void Update()
+    {
+        Character.Turn();
+        //Викликаємо функцію ходьби у персонажа
+        Character.Walk();
+
+        //Викликаємо у персонажа метод стрибку
+        if (Input.GetButtonDown("Jump"))
+            Character.Jump();
+    }
+}
